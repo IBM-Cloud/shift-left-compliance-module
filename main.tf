fd1ed3b283f276f5f37e8c22221aa501f61a932d
Chuck Cox <chuckc@us.ibm.com>
2021-06-03 17:02:31 -0500

    new: resources
    Adding the resources needed to create the Kube cluster and other things.
    
    Taken from here: https://github.com/IBM-Cloud/Scalable-web-app-node/blob/master/terraform/secure-kube/main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,35 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+resource "random_string" "random" {
+  length = 4
+  min_lower = 4
+}
+
+data "ibm_resource_group" "group" {
+  name = var.resource_group
+}
+
+provider "ibm" {
+  ibmcloud_api_key   = var.ibmcloud_api_key
+}
+
+resource "ibm_container_cluster" "cluster" {
+  name              = var.cluster_name
+  datacenter        = var.datacenter
+  default_pool_size = var.default_pool_size
+  machine_type      = var.machine_type
+  hardware          = var.hardware
+  kube_version      = var.kube_version
+  public_vlan_id    = var.public_vlan_num
+  private_vlan_id   = var.private_vlan_num
+  resource_group_id = data.ibm_resource_group.group.id
+}
+
 resource "null_resource" "create_kubernetes_toolchain" {
   provisioner "local-exec" {
-    command = "${path.cwd}/.terraform/modules/ibm-kubernetes-toolchain/scripts/create-toolchain.sh"
+    command = "${path.cwd}/.terraform/modules/shift-left-compliance-module/scripts/create-toolchain.sh"
 
     environment={
       REGION            =  var.region
